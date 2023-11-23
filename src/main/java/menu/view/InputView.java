614e40fca01ea1a91b21a1e953285246076e5f8a
jinny-l <jinny.l.dev@gmail.com>
2023-11-23 18:20:54 +0900

    feat: 코치 이름 및 못 먹는 메뉴 입력 기능 구현

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,60 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class InputView {
+
+    private static final String SPLIT_REGEX = ",";
+
+    private InputView() {
+    }
+
+    public static List<String> readNames() {
+        System.out.printf("%n코치의 이름을 입력해 주세요. (%s 로 구분)%n", SPLIT_REGEX);
+
+        String input = readLine();
+        validateHasInput(input);
+        return stringToList(input);
+    }
+
+    public static List<String> readBannedMenu(String coachName) {
+        System.out.printf("%n%s(이)가 못 먹는 메뉴를 입력해 주세요%n", coachName);
+
+        String input = readLine();
+        return stringToList(input);
+    }
+
+    private static String readLine() {
+        return Console.readLine().strip();
+    }
+
+    private static boolean validateHasInput(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("입력 값이 비어있습니다.");
+        }
+        return true;
+    }
+
+    private static List<String> stringToList(String input) {
+        if (input.equals("")) {
+            return Collections.emptyList();
+        }
+
+        List<String> split = Stream.of(input.split(SPLIT_REGEX))
+                .filter(InputView::validateHasInput)
+                .map(String::strip)
+                .toList();
+
+        validateDelimiter(input, split);
+        return split;
+    }
+
+    private static void validateDelimiter(String input, List<String> split) {
+        if (split.size() > 1 && !input.contains(SPLIT_REGEX)) {
+            throw new IllegalArgumentException(String.format("입력 값은 \"%s\"로 구분해주세요", SPLIT_REGEX));
+        }
+    }
+}
